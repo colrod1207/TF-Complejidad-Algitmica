468b10d1a51c8895f7c0512a82dadd81f50efd05
leonardo <u202124304@upc.edu.pe>
2024-06-23 21:02:01 -0500

    chore: Reajustar tamaño de las fuentes

--- app2.py
+++ app2.py
@@ -192,7 +192,7 @@ start_node_label.grid(row=1, column=0, sticky=tk.W, pady=(0, 5))
 start_node_entry = ttk.Entry(frame, width=10, font=("Helvetica", 12))
 start_node_entry.grid(row=1, column=1, sticky=tk.W, pady=(0, 5))
 
-end_node_label = ttk.Label(frame, text="Nodo de Fin:", font=("Helvetica", 12))
+end_node_label = ttk.Label(frame, text="Nodo de Fin:", font=("Helvetica", 18))
 end_node_label.grid(row=2, column=0, sticky=tk.W, pady=(0, 5))
 end_node_entry = ttk.Entry(frame, width=10, font=("Helvetica", 12))
 end_node_entry.grid(row=2, column=1, sticky=tk.W, pady=(0, 5))
@@ -203,7 +203,7 @@ calculate_button.grid(row=3, column=0, columnspan=2, pady=(10, 5))
 show_graph_button = ttk.Button(frame, text="Mostrar Grafo Completo", command=show_full_graph)
 show_graph_button.grid(row=4, column=0, columnspan=2, pady=(5, 10))
 
-result_label = ttk.Label(frame, text="", wraplength=400, font=("Helvetica", 12))
+result_label = ttk.Label(frame, text="", wraplength=400, font=("Helvetica", 15))
 result_label.grid(row=5, column=0, columnspan=2, pady=(0, 10))
 
 fig = plt.figure(figsize=(5, 4))
